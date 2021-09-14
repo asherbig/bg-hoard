ed75ae1b89e2e8714978a85be3a7011a461c87ef
Alex Herbig <aherbigs@gmail.com>
2021-09-14 13:46:18 -0400

    Finish lab 5

---
+++ apps/store/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { formatRating } from '@bg-hoard/store/util-formatters';
+
+import { getAllGames } from '../fake-api';
+
+@Component({
+  selector: 'bg-hoard-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Board Game Hoard';
+  games = getAllGames();
+  formatRating = formatRating;
+}
---
+++ apps/store/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    MatCardModule,
+    StoreUiSharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
